80676e3ffbf6da23adbfa4888799e6b1c9d51b70
Lina-SHU <sakura0477@gmail.com>
2021-08-04 21:34:46 +0800

    update readonly input

--- app/assets/js/all.js
+++ app/assets/js/all.js
@@ -75,8 +75,8 @@ if (document.title === "首頁"){
     } else if (value === 'edit') {
       accountName.value = "Emir Wicks";
       email.value = "emir.wicks@mail.com";
-      accountName.classList.add('readonlyStyle');
-      email.classList.add('readonlyStyle');
+      accountName.setAttribute('readonly', true);
+      email.setAttribute('readonly', true);
     }
   }
   // verified
@@ -99,8 +99,8 @@ if (document.title === "首頁"){
 
   // remove
   function removeSet(){
-    accountName.classList.remove('readonlyStyle');
-    email.classList.remove('readonlyStyle');
+    accountName.removeAttribute('readonly');
+    email.removeAttribute('readonly');
     notVerified.classList.remove('d-block');
     isVerified.classList.remove('d-block');
     isDectivate.classList.remove('d-block');
